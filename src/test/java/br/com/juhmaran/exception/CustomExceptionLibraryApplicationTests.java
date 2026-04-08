1633eca6dc293645a0d87e2db9d6a1f5c56b5a91
JuhMaran <julianemaran@gmail.com>
2026-04-08 09:34:38 -0300

    initial project

---
+++ src/test/java/br/com/juhmaran/exception/CustomExceptionLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.juhmaran.exception;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomExceptionLibraryApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
